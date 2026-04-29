5100d83a1ae00f3245d96cd511976ae74fcb86de
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2026-04-29 14:08:44 -0300

    Preparando o vite config para hospedagem

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
-  base: '/FilmesAPIVue/',
+  base: '/APIFilmesVue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
