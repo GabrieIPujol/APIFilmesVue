66aafe5b3f2429651fa9ee5201f422a61c27a1ba
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2026-05-05 10:41:23 -0300

    Fixing base link to deploy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
-  base: '/APIFilmesVue/',
+  base: '/CineRetro/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
